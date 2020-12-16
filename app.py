db4fa7b64288735103a7f3349c73863f853328e1
Daniel Mbugua <daniel.mbugua@savannahinformatics.com>
2020-12-16 10:14:23 +0300

    Configure docker

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask
-from auto_cell import get_programming_language
+from awe_pro import auto_cell
 
 app = Flask(__name__)
 
 @app.route("/")
-def generate_buzz():
+def get_favorite_programming_language():
     question = 'Your favorite random programming language? '
     page = '<html><body><h1>'
     page += question
-    page += get_programming_language()
+    page += auto_cell.get_programming_language()
     page += '</h1></body></html>'
     return page
 
